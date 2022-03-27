451209c209688ea980596c1d752e0291bf1a95eb
Jason Vanstone <jason@chameleoncreative.com>
2022-03-27 11:27:26 -0400

    added custom css for the the-challenge

--- functions.php
+++ functions.php
@@ -176,3 +176,23 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+
+// add arrows to menu parent 
+function oenology_add_menu_parent_class( $items ) {
+ 
+ $parents = array();
+ foreach ( $items as $item ) {
+ if ( $item->menu_item_parent && $item->menu_item_parent > 0 ) {
+ $parents[] = $item->menu_item_parent;
+ }
+ }
+ 
+ foreach ( $items as $item ) {
+ if ( in_array( $item->ID, $parents ) ) {
+ $item->classes[] = 'has-children';
+ }
+ }
+ 
+ return $items;
+}
+add_filter( 'wp_nav_menu_objects', 'oenology_add_menu_parent_class' );
